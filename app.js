080ea42369fbc23ea0305abdd2e4a1908edae46d
DarthVanger <darthvanger@gmail.com>
2019-10-05 18:51:43 +0300

    Add a button to create a folder

--- app.js
+++ app.js
@@ -6,8 +6,26 @@ const app = {
 
     const googleDriveApiJsSimple = new GoogleDriveApiJsSimple(gapi);
 
+    const createFolderButton = document.createElement('button');
+    createFolderButton.onclick = createFolder;
+    createFolderButton.innerHTML = 'Create folder';
+    document.body.append(createFolderButton);
+
     console.log('googleDriveApiJsSimple: ', googleDriveApiJsSimple);
-  }
+    console.log('createFolderButton: ', createFolderButton);
+
+    function createFolder() {
+      console.log('creating folder :)');
+      googleDriveApiJsSimple.createDirectory({
+        name: 'google-drive-api-js-simple-demo',
+      }).then(() => {
+        console.log('dir created success. Args: ', arguments);
+      }).catch(() => {
+        console.log('dir cretion failed!. Args: ', arguments);
+      });
+    }
+  },
+
 };
 
 export default app;
